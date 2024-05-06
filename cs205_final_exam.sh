a21c418609ac420a3d4ebc5209aa786daf7aa4e9
coingove05 <163959457+coingove05@users.noreply.github.com>
2024-05-06 13:38:19 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -5,6 +5,16 @@
 #    Avg. HP: [VALUE]
 #    Avg. Attack: [VALUE]
 # ===== END SUMMARY =====
+#!/bin/bash
+filename=$1
+total=$(awk 'END {print NR}' $filename)
+avg_hp=$(awk '{sum += $3} END {print sum / NR}' $filename)
+avg_attack=$(awk '{sum += $4} END {print sum / NR}' $filename)
+echo "======= SUMMARY OF $filename ======"
+echo "# Total Pokemon: $total"
+echo "# Avg. HP: $avg_hp"
+echo "# Avg. Attack: $avg_attack"
+echo "# ======= END SUMMARY =======#"
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
